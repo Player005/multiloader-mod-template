220bf9068ba671b48b406eec8674d0f9e9606138
player005 <me@player005.net>
2024-11-01 16:46:04 +0100

    add example code

--- common/src/main/java/net/yourpackage/yourmod/MyMod.java
+++ common/src/main/java/net/yourpackage/yourmod/MyMod.java
@@ -4,6 +4,8 @@ public class MyMod {
     static final String modID = "my_mod_id";
 
     static void init() {
+        System.out.println("Hi from example mod!");
+
         // Your common initialisation code here
     }
 }
