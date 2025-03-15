1bf0086f9934be173530ae9fbcefe90c5368922b
player005 <me@player005.net>
2025-03-15 22:35:19 +0100

    introduce unified load conditions & automatic fluid conversions

--- neoforge/build.gradle.kts
+++ neoforge/build.gradle.kts
@@ -72,5 +72,68 @@ tasks {
             @Suppress("UNCHECKED_CAST")
             expand(inputs.properties["property_map"] as Map<String, String>)
         }
+
+        // handle platform conversions if enabled
+        inputs.property("handle_fluid_conversion", rootProject.properties["handle_fluid_conversion"])
+        inputs.property("unified_load_conditions", rootProject.properties["unified_load_conditions"])
+
+        filesMatching("data/**/*.json") {
+            if (inputs.properties["handle_fluid_conversion"].toString().toBoolean())
+                convertFluidUnitsForge()
+            if (inputs.properties["unified_load_conditions"].toString().toBoolean())
+                processUnifiedLoadConditionsForge()
+        }
+    }
+}
+
+
+fun ContentFilterable.convertFluidUnitsForge() {
+    filter { line ->
+        var result = line.replace(""""(\d*\.?\d*)_millibuckets"""".toRegex(), "$1")
+        val regex = """"(\d*\.?\d*)_droplets"""".toRegex()
+        regex.find(line)?.let { result = result.replace(regex, (it.groups[1]!!.value.toInt() / 81).toString()) }
+        result
+    }
+}
+
+fun ContentFilterable.processUnifiedLoadConditionsForge() {
+    filter { line ->
+        // I am very sorry for anyone who tries to read or understand this
+        line.replace(
+            """^(\s*)"load_conditions":""".toRegex(),
+            "$1\"neoforge:conditions\":"
+        )
+            .replace(
+                """^(\s*\{?\s*)"condition":\s*"mod_loaded"""".toRegex(),
+                "$1\"type\": \"neoforge:mod_loaded\""
+            )
+            .replace(
+                """^(\s*)"mod":\s*"(.*)"""".toRegex(),
+                "$1\"modid\": \"$2\""
+            )
+            .replace(
+                """^(\s*\{?\s*)"condition":\s*"and"""".toRegex(),
+                "$1\"type\": \"neoforge:and\""
+            )
+            .replace(
+                """^(\s*\{?\s*)"condition":\s*"or"""".toRegex(),
+                "$1\"type\": \"neoforge:or\""
+            )
+            .replace(
+                """^(\s*\{?\s*)"condition":\s*"is_fabric"""".toRegex(),
+                "$1\"type\": \"neoforge:false\""
+            )
+            .replace(
+                """^(\s*\{?\s*)"condition":\s*"is_neoforge"""".toRegex(),
+                "$1\"type\": \"neoforge:true\""
+            )
+            .replace(
+                """^(\s*\{?\s*)"condition":\s*"true"""".toRegex(),
+                "$1\"type\": \"neoforge:true\""
+            )
+            .replace(
+                """^(\s*\{?\s*)"condition":\s*"false"""".toRegex(),
+                "$1\"type\": \"neoforge:false\""
+            )
     }
 }
