525302528bbbeb1c7a271e873b6a3f6e7493796e
player005 <me@player005.net>
2025-03-16 13:11:56 +0100

    fix config cache

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -54,7 +54,10 @@ tasks {
 
         // the properties listed here can be used in the fabric.mod.json
         val properties =
-            listOf("mc_versions_fabric", "mod_version", "mod_id", "mod_name", "mod_description", "mod_authors", "mod_license")
+            listOf(
+                "mc_versions_fabric", "mod_version", "mod_id", "mod_name",
+                "mod_description", "mod_authors", "mod_license"
+            )
 
         val map = mutableMapOf<String, String>()
         properties.forEach { map[it] = rootProject.properties[it].toString() }
@@ -65,15 +68,16 @@ tasks {
             expand(inputs.properties["property_map"] as Map<String, String>)
         }
 
-        // handle platform conversions if enabled
-        inputs.property("handle_fluid_conversion", rootProject.properties["handle_fluid_conversion"])
-        inputs.property("unified_load_conditions", rootProject.properties["unified_load_conditions"])
+        // do fluid unit conversions and unified load condition processing if enabled
+        // see `Platform conversions.md` for more information
+        inputs.property("convert_fluid_units", rootProject.properties["convert_fluid_units"] == "true")
+        inputs.property("unified_load_conditions", rootProject.properties["unified_load_conditions"] == "true")
 
         filesMatching("data/**/*.json") {
-            if (inputs.properties["handle_fluid_conversion"].toString().toBoolean())
-                convertFluidUnitsFabric()
-            if (inputs.properties["unified_load_conditions"].toString().toBoolean())
-                processUnifiedLoadConditionsFabric()
+            if (inputs.properties["convert_fluid_units"] as Boolean)
+                filter(FabricConversions.fluidUnitConverter)
+            if (inputs.properties["unified_load_conditions"] as Boolean)
+                filter(FabricConversions.unifiedLoadConditionProcessor)
         }
     }
 
@@ -86,18 +90,17 @@ tasks {
     }
 }
 
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+object FabricConversions {
 
-fun ContentFilterable.convertFluidUnitsFabric() {
-    filter { line ->
+    val fluidUnitConverter = Transformer<String?, String> { line ->
         var result = line.replace(""""(\d*\.?\d*)_droplets"""".toRegex(), "$1")
         val regex = """"(\d*\.?\d*)_millibuckets"""".toRegex()
         regex.find(line)?.let { result = result.replace(regex, (it.groups[1]!!.value.toInt() * 81).toString()) }
         result
     }
-}
 
-fun AbstractCopyTask.processUnifiedLoadConditionsFabric() {
-    filter { line ->
+    val unifiedLoadConditionProcessor = Transformer<String?, String> { line ->
         // I am very sorry for anyone who tries to read or understand this
         line.replace(
             """^(\s*)"load_conditions":""".toRegex(),
--- neoforge/build.gradle.kts
+++ neoforge/build.gradle.kts
@@ -61,7 +61,10 @@ tasks {
 
         // the properties listed here can be used in the mods.toml
         val properties =
-            listOf("mc_versions_neo", "neo_loader_version_range", "mod_version", "mod_id", "mod_name", "mod_description", "mod_authors", "mod_license")
+            listOf(
+                "mc_versions_neo", "neo_loader_version_range", "mod_version", "mod_id", "mod_name",
+                "mod_description", "mod_authors", "mod_license"
+            )
 
         // store a map of the properties so the configuration cache can be used
         val map = mutableMapOf<String, String>()
@@ -73,31 +76,32 @@ tasks {
             expand(inputs.properties["property_map"] as Map<String, String>)
         }
 
-        // handle platform conversions if enabled
-        inputs.property("handle_fluid_conversion", rootProject.properties["handle_fluid_conversion"])
-        inputs.property("unified_load_conditions", rootProject.properties["unified_load_conditions"])
+        // do fluid unit conversions and unified load condition processing if enabled
+        // see `Platform conversions.md` for more information
+        inputs.property("convert_fluid_units", rootProject.properties["convert_fluid_units"] == "true")
+        inputs.property("unified_load_conditions", rootProject.properties["unified_load_conditions"] == "true")
 
         filesMatching("data/**/*.json") {
-            if (inputs.properties["handle_fluid_conversion"].toString().toBoolean())
-                convertFluidUnitsForge()
-            if (inputs.properties["unified_load_conditions"].toString().toBoolean())
-                processUnifiedLoadConditionsForge()
+            if (inputs.properties["convert_fluid_units"] as Boolean)
+                filter(FabricConversions.fluidUnitConverter)
+            if (inputs.properties["unified_load_conditions"] as Boolean)
+                filter(FabricConversions.unifiedLoadConditionProcessor)
         }
     }
 }
 
 
-fun ContentFilterable.convertFluidUnitsForge() {
-    filter { line ->
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+object FabricConversions {
+
+    val fluidUnitConverter = Transformer<String?, String> { line ->
         var result = line.replace(""""(\d*\.?\d*)_millibuckets"""".toRegex(), "$1")
         val regex = """"(\d*\.?\d*)_droplets"""".toRegex()
         regex.find(line)?.let { result = result.replace(regex, (it.groups[1]!!.value.toInt() / 81).toString()) }
         result
     }
-}
 
-fun ContentFilterable.processUnifiedLoadConditionsForge() {
-    filter { line ->
+    val unifiedLoadConditionProcessor = Transformer<String?, String> { line ->
         // I am very sorry for anyone who tries to read or understand this
         line.replace(
             """^(\s*)"load_conditions":""".toRegex(),
