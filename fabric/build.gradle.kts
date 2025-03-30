956998071f3772011b5110bee025341c615d32bc
player005 <me@player005.net>
2025-03-30 17:55:54 +0200

    small fix

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -130,7 +130,7 @@ object FabricConversions {
             )
             .replace(
                 """^(\s*)"not":\s*\{""".toRegex(),
-                "$1\"type\": \"fabric:not\",\n$1\"value\": {"
+                "$1\"condition\": \"fabric:not\",\n$1\"value\": {"
             )
             .replace(
                 """^(\s*\{?\s*)"condition":\s*"and"""".toRegex(),
